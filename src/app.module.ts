4abf7a9a688a9976e55952a737cffc8b9c08650d
adkshishir <adhikarishishir50@gmail.com>
2025-05-01 12:33:31 +0545

    Add initial backend modules and DTOs for travel site features

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,33 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
+import { ActivitiesModule } from './activities/activities.module';
+import { DestinationsModule } from './destinations/destinations.module';
+import { PackagesModule } from './packages/packages.module';
+import { SiteInfoModule } from './site-info/site-info.module';
+import { BookingModule } from './booking/booking.module';
+import { CarouselsModule } from './carousels/carousels.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { FaqModule } from './faq/faq.module';
 
 @Module({
   imports: [
     UploadModule,
     PrismaModule,
+    ActivitiesModule,
+    DestinationsModule,
     AuthModule,
+    CarouselsModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'uploads'), // Path to your uploads folder
       serveRoot: '/uploads', // URL prefix for static files
     }),
+    PackagesModule,
+    SiteInfoModule,
+    BookingModule,
+    ReviewsModule,
+    FaqModule,
+    // SeoModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
