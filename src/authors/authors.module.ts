deb41bd6e99fd4707070ef0b290d0b7859cf63a1
adkshishir <adhikarishishir50@gmail.com>
2025-07-23 09:07:09 +0545

    fix: injection error on blog and author

--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthorsService } from './authors.service';
 import { AuthorsController } from './authors.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthorsController],
-  providers: [AuthorsService, PrismaService],
+  providers: [AuthorsService, PrismaService,JwtService],
 })
 export class AuthorsModule {}
--- src/blogs/blogs.module.ts
+++ src/blogs/blogs.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { BlogsService } from './blogs.service';
 import { BlogsController } from './blogs.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import { UploadService } from 'src/upload/upload.service';
 
 @Module({
   controllers: [BlogsController],
-  providers: [BlogsService, PrismaService],
+  providers: [BlogsService, PrismaService,JwtService, UploadService],
 })
 export class BlogsModule {} 
